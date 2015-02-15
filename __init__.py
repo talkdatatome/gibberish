51fb792f63b6f041cf76ba5d54766037bede0af3
Tim Trudell <timtrudell@gmail.com>
2015-02-15 12:14:59 -0500

    Added initial data to fit model if table does not exist. The model initialization should happen here too.

--- __init__.py
+++ __init__.py
@@ -13,24 +13,34 @@ celery.conf.update(app.config)
 from sqlalchemy import Column, Integer, String, Boolean, create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///gibberish.db')
-Base = declarative_base()
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
+from sqlalchemy.engine import reflection
+from sqlalchemy.exc import OperationalError
 
-#session = Session()
+Base = declarative_base()
 class GibData(Base):
     __tablename__='gib'
     _id = Column('id', Integer, primary_key=True)
     text = Column('text', String(length=500))
     handFlag = Column('handFlag', Boolean)
 
-    def __init__(self):
-        """Intialize db with simple list of gibberish and nongibberish."""
-        pass
-
     def __repr__(self):
         return("<gib_data(_id='%s', text='%s', handFlag='%s')>" % (self._id, self.text, self.handFlag))
 
+engine = create_engine('sqlite:///gibberish.db')
+insp = reflection.Inspector.from_engine(engine)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+try:
+    if session.query(GibData).count() == 0:
+        init_data = [("i am not gibberish", False), ("this is also a real answer", False), ("baglkd zzzzzzzz", True), ("asdfkadnsknn", True), ("", True)]
+        for x in init_data:
+            new_data = GibData(text=x[0], handFlag=x[1])
+            session.add(new_data)
+        session.commit()
+        session.close()
+except OperationalError:
+    pass
 
 from gibberish import views
